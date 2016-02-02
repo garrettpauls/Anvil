c34658a5d41f0c35c59354b0b67ba1c988a61c8f
Garrett Pauls <garrett.pauls@gmail.com>
2016-02-01 18:45:15 -0700

    Added basic margins and styling

---
+++ Source/Anvil/Framework/Xaml/Converters/TextToUpperCaseConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Anvil.Framework.Xaml.Converters
+{
+    public sealed class TextToUpperCaseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value as string;
+
+            if(str == null)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            return str.ToUpper(culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
