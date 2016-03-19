356719364b6b2f9dcc48fbd9daae41c0669da048
Garrett Pauls <garrett.pauls@gmail.com>
2016-03-19 13:13:19 -0600

    Added GeometryButton for simpler buttons with images

---
+++ Source/Anvil/Controls/GeometryButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace Anvil.Controls
+{
+    [ContentProperty(nameof(Content))]
+    public class GeometryButton : Button
+    {
+        public static readonly DependencyProperty GeometryProperty = DependencyProperty.Register(
+            "Geometry", typeof(Geometry), typeof(GeometryButton), new PropertyMetadata(default(Geometry)));
+
+        static GeometryButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(GeometryButton), new FrameworkPropertyMetadata(typeof(GeometryButton)));
+        }
+
+        public Geometry Geometry
+        {
+            get { return (Geometry) GetValue(GeometryProperty); }
+            set { SetValue(GeometryProperty, value); }
+        }
+    }
+}
