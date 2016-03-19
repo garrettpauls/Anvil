101d894a12b4a9dfc3058fa6dda6d119b0d7045a
Garrett Pauls <garrett.pauls@gmail.com>
2016-03-19 13:59:33 -0600

    Added file browser lookup buttons

---
+++ Source/Anvil/Framework/FileFilterBuilder.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Anvil.Framework
+{
+    public sealed class FileFilterBuilder
+    {
+        private readonly StringBuilder mBuilder = new StringBuilder();
+
+        private void _AppendFilter(string fullFilter)
+        {
+            if(mBuilder.Length > 0)
+            {
+                mBuilder.Append("|");
+            }
+            mBuilder.Append(fullFilter);
+        }
+
+        public string ToFileFilter()
+        {
+            return mBuilder.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToFileFilter();
+        }
+
+        public FileFilterBuilder With(string description, params string[] extensions)
+        {
+            var combinedExtensions = string.Join(";", extensions.Select(ext => $"*{ext}"));
+            _AppendFilter($"{description}|{combinedExtensions}");
+            return this;
+        }
+
+        public FileFilterBuilder WithAllFiles()
+        {
+            _AppendFilter("All files|*");
+            return this;
+        }
+    }
+}
--- Source/Anvil/Views/ConfigurationUI/LaunchItemEditView.xaml.cs
+++ Source/Anvil/Views/ConfigurationUI/LaunchItemEditView.xaml.cs
@@ -1,4 +1,11 @@
-﻿using Anvil.Framework.MVVM;
+﻿using System.Windows;
+
+using Anvil.Framework;
+using Anvil.Framework.MVVM;
+
+using Microsoft.Win32;
+
+using Ookii.Dialogs.Wpf;
 
 namespace Anvil.Views.ConfigurationUI
 {
@@ -7,6 +14,33 @@ namespace Anvil.Views.ConfigurationUI
         public LaunchItemEditView()
         {
             InitializeComponent();
+        }
+
+        private void _LookupFile(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.FileName = FileTextBox.Text ?? "";
+            dialog.Multiselect = false;
+            dialog.CheckFileExists = true;
+            dialog.Filter = new FileFilterBuilder()
+                .With("Executable files", ".exe", ".bat", ".cmd", ".com", ".lnk", ".ps1")
+                .WithAllFiles()
+                .ToFileFilter();
+
+            if(dialog.ShowDialog(Window.GetWindow(this)) ?? false)
+            {
+                FileTextBox.Text = dialog.FileName;
+            }
+        }
+
+        private void _LookupWorkingDirectory(object sender, RoutedEventArgs e)
+        {
+            var dialog = new VistaFolderBrowserDialog();
+
+            if(dialog.ShowDialog(Window.GetWindow(this)) ?? false)
+            {
+                WorkingDirectoryTextBox.Text = dialog.SelectedPath;
+            }
         }
     }
 
