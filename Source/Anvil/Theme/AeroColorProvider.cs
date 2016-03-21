767f66990b0bad38ac966b550704f60364a46474
Garrett Pauls <garrett.pauls@gmail.com>
2016-03-20 19:35:35 -0600

    Added MahApps UI and refactored updating to be automatic.

---
+++ Source/Anvil/Theme/AeroColorProvider.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Anvil.Theme
+{
+    /// <summary>
+    ///     Based on https://gist.github.com/usagirei/a2cfe768e4cf5fb34694
+    /// </summary>
+    public sealed class AeroColorProvider : DependencyObject, IDisposable
+    {
+        public static readonly DependencyProperty Accent1Property = DependencyProperty.Register
+            (nameof(Accent), typeof(Color), typeof(AeroColorProvider), new PropertyMetadata(default(Color)));
+
+        public static readonly DependencyProperty Accent2Property = DependencyProperty.Register
+            (nameof(Accent2), typeof(Color), typeof(AeroColorProvider), new PropertyMetadata(default(Color)));
+
+        public static readonly DependencyProperty Accent3Property = DependencyProperty.Register
+            (nameof(Accent3), typeof(Color), typeof(AeroColorProvider), new PropertyMetadata(default(Color)));
+
+        public static readonly DependencyProperty Accent4Property = DependencyProperty.Register
+            (nameof(Accent4), typeof(Color), typeof(AeroColorProvider), new PropertyMetadata(default(Color)));
+
+        public static readonly DependencyProperty HighlightProperty = DependencyProperty.Register
+            (nameof(Highlight), typeof(Color), typeof(AeroColorProvider), new PropertyMetadata(default(Color)));
+
+        private readonly IDisposable mUpdateColorsSubscription;
+
+        public AeroColorProvider()
+        {
+            mUpdateColorsSubscription = Observable
+                .FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
+                    handler => SystemParameters.StaticPropertyChanged += handler,
+                    handler => SystemParameters.StaticPropertyChanged -= handler)
+                .Where(evt => evt.EventArgs.PropertyName == nameof(SystemParameters.WindowGlassColor))
+                .Subscribe(evt => _UpdateColors());
+
+            _UpdateColors();
+        }
+
+        public Color Accent
+        {
+            get { return (Color) GetValue(Accent1Property); }
+            set { SetValue(Accent1Property, value); }
+        }
+
+        public Color Accent2
+        {
+            get { return (Color) GetValue(Accent2Property); }
+            set { SetValue(Accent2Property, value); }
+        }
+
+        public Color Accent3
+        {
+            get { return (Color) GetValue(Accent3Property); }
+            set { SetValue(Accent3Property, value); }
+        }
+
+        public Color Accent4
+        {
+            get { return (Color) GetValue(Accent4Property); }
+            set { SetValue(Accent4Property, value); }
+        }
+
+        public Color Highlight
+        {
+            get { return (Color) GetValue(HighlightProperty); }
+            set { SetValue(HighlightProperty, value); }
+        }
+
+        private void _HandleSystemParameterChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if(e.PropertyName == nameof(SystemParameters.WindowGlassColor))
+            {
+                _UpdateColors();
+            }
+        }
+
+        private void _UpdateColors()
+        {
+            var aero = SystemParameters.WindowGlassColor;
+
+            Highlight = _SetIntensity(aero, 1.1f);
+            Accent = _SetIntensity(aero, 0.95f);
+            Accent2 = _SetIntensity(aero, 0.8f);
+            Accent3 = _SetIntensity(aero, 0.65f);
+            Accent4 = _SetIntensity(aero, 0.5f);
+        }
+
+        public void Dispose()
+        {
+            mUpdateColorsSubscription.Dispose();
+        }
+
+        private static byte _ClipByte(float input)
+        {
+            return (byte) ((input < 0 ? 0 : input > 1 ? 1 : input)*255);
+        }
+
+        private static Color _SetIntensity(Color input, float power)
+        {
+            var newR = (input.R/255f)*power;
+            var newG = (input.G/255f)*power;
+            var newB = (input.B/255f)*power;
+
+            return Color.FromRgb(_ClipByte(newR), _ClipByte(newG), _ClipByte(newB));
+        }
+    }
+}
---
+++ Source/Anvil/Views/UpdateViewModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Text;
+
+using Anvil.Framework;
+using Anvil.Framework.ComponentModel;
+using Anvil.Framework.MVVM;
+
+using Autofac.Extras.NLog;
+
+using ReactiveUI;
+
+using Squirrel;
+
+namespace Anvil.Views
+{
+    public sealed class UpdateViewModel : RoutableViewModel
+    {
+        private readonly ILogger mLog;
+        private readonly IUpdateManager mUpdateManager;
+        private bool mIsUpdateAvailable;
+        private string mReleaseNotes;
+        private UpdateInfo mUpdateInfo;
+
+        public UpdateViewModel(IUpdateManager updateManager, IScreen hostScreen, ILogger log) : base("update", hostScreen)
+        {
+            Guard.AgainstNullArgument(nameof(updateManager), updateManager);
+            Guard.AgainstNullArgument(nameof(hostScreen), hostScreen);
+            Guard.AgainstNullArgument(nameof(log), log);
+
+            mLog = log;
+
+            mUpdateManager = updateManager;
+
+            ApplyUpdateCommand = ReactiveCommand.Create(this.WhenAnyValue(x => x.IsUpdateAvailable));
+            ApplyUpdateCommand
+                .ObserveOn(ThreadPoolScheduler.Instance)
+                .Subscribe(_ApplyUpdate).TrackWith(Disposables);
+
+            CheckForUpdatesCommand = ReactiveCommand.Create();
+            CheckForUpdatesCommand
+                .ObserveOn(ThreadPoolScheduler.Instance)
+                .Subscribe(_CheckForUpdates).TrackWith(Disposables);
+        }
+
+        public ReactiveCommand<object> ApplyUpdateCommand { get; }
+
+        public ReactiveCommand<object> CheckForUpdatesCommand { get; }
+
+        public bool IsUpdateAvailable
+        {
+            get { return mIsUpdateAvailable; }
+            private set { this.RaiseAndSetIfChanged(ref mIsUpdateAvailable, value); }
+        }
+
+        public string ReleaseNotes
+        {
+            get { return mReleaseNotes; }
+            private set { this.RaiseAndSetIfChanged(ref mReleaseNotes, value); }
+        }
+
+        private async void _ApplyUpdate(object _)
+        {
+            IsUpdateAvailable = false;
+
+            var update = mUpdateInfo;
+            if(update == null)
+            {
+                return;
+            }
+
+            await mUpdateManager.ApplyReleases(update);
+        }
+
+        private async void _CheckForUpdates(object _)
+        {
+            mLog.Info("Checking for updates...");
+
+            mUpdateInfo = await mUpdateManager.CheckForUpdate();
+            IsUpdateAvailable =
+                mUpdateInfo != null &&
+                mUpdateInfo.FutureReleaseEntry != null && mUpdateInfo.CurrentlyInstalledVersion != null &&
+                mUpdateInfo.FutureReleaseEntry.Version != mUpdateInfo.CurrentlyInstalledVersion.Version;
+
+            if(IsUpdateAvailable)
+            {
+                ReleaseNotes = _CreateReleaseNotes(mUpdateInfo);
+            }
+        }
+
+        private static string _CreateReleaseNotes(UpdateInfo update)
+        {
+            var notes = update.FetchReleaseNotes();
+            var orderedNotes = notes.OrderByDescending(note => note.Key.Version);
+
+            var builder = new StringBuilder();
+
+            foreach(var note in orderedNotes)
+            {
+                builder.AppendLine(note.Key.Version.ToString());
+                builder.AppendLine();
+
+                builder.AppendLine(note.Value);
+                builder.AppendLine();
+            }
+
+            return builder.ToString();
+        }
+    }
+}
